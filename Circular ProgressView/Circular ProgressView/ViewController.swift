d2387aa4431c5aa52ae9c2fc75396d1d245d6c0f
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-07-12 14:55:54 +0530

    added code

--- Circular ProgressView/Circular ProgressView/ViewController.swift
+++ Circular ProgressView/Circular ProgressView/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        let circularProgress = CircularProgress(frame: CGRect(x: 10.0, y: 30.0, width: 134.0, height: 134.0))
+        // circularProgress.layer.borderWidth = 5
+        // circularProgress.backgroundColor = UIColor.white
+        circularProgress.progressColor =  UIColor(red: 0.00, green: 0.78, blue: 0.71, alpha: 1)
+        circularProgress.trackColor = UIColor(red: 52.0/255.0, green: 141.0/255.0, blue: 252.0/255.0, alpha: 0.6)
+        circularProgress.tag = 101
+        circularProgress.center = self.view.center
+        self.view.addSubview(circularProgress)
+        
+        //animate progress
+        self.perform(#selector(animateProgress), with: nil, afterDelay: 0.3)
     }
-
-
+    
+    @objc func animateProgress() {
+        let cp = self.view.viewWithTag(101) as! CircularProgress
+        cp.setProgressWithAnimation(duration: 1.0, value: 0.8)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
-
